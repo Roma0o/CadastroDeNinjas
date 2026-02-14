82bd410467a4ebb38d0f1277c0c5bfd77f032fd4
Maria José <lucasromao2006@icloud.com>
2026-02-14 12:37:57 -0300

    Criado a controller

---
+++ src/main/java/com/treino/CadastroDeNinjas/Controller.java
@@ -0,0 +1,21 @@
+package com.treino.CadastroDeNinjas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Romão
+ */
+
+@RestController
+@RequestMapping
+public class Controller {
+    
+    @GetMapping("/boasvindas")
+    public String boasVindas(){
+    return "Essa é minha primeira mensagem nessa rota";
+    }
+    
+}
